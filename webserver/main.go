9d8264ffd316640d0f60e24d76a3357397c3ed26
yukiel <kennylevu1@gmail.com>
2025-09-15 14:39:50 -0700

    feat(webserver): server home page with go

--- webserver/main.go
+++ webserver/main.go
@@ -10,5 +10,9 @@ func main() {
 		fmt.Fprint(w, "Web server is running!")
 	})
 
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "home.html")
+	})
+
 	http.ListenAndServe(":3000", nil)
 }
